b93fdf78a3f04be1312001be8c90cc3d98c6d90d
cafeConLeche <cafe@conleche.com>
2021-04-20 09:06:07 -0300

    Creacion de Controlador Conference. Se realiza con homepage renderiza nombre

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    // public function index(): Response
+    public function index(Request $request): Response
+    {
+        // return $this->render('conference/index.html.twig', [
+        //     'controller_name' => 'ConferenceController',
+        // ]);
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+                return new Response(<<<EOF
+                    <html>
+                        <body>
+                            $greet
+                            <img src="/images/under-construction.gif" />
+                        </body>
+                    </html>
+                    EOF
+                );
+    }
+}
