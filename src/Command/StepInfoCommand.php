a5e87c200aac06e0cb2ef2da09c86d3d82139330
cafeConLeche <cafe@conleche.com>
2021-05-12 09:37:00 -0300

    >> Capitulo: 21  > Paso: 21.7-8
    .: 21.7 no se realizo, xq no ha funcionado la cache.

--- src/Command/StepInfoCommand.php
+++ src/Command/StepInfoCommand.php
@@ -8,11 +8,23 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
+use Symfony\Contracts\Cache\CacheInterface;
+
 
 class StepInfoCommand extends Command
 {
     protected static $defaultName = 'app:step:info';
     protected static $defaultDescription = 'Add a short description for your command';
+
+    private $cache;
+
+    public function __construct(CacheInterface $cache)
+    {
+        $this->cache = $cache;
+
+        parent::__construct();
+    }
 
     protected function configure()
     {
@@ -25,18 +37,27 @@ class StepInfoCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        // $io = new SymfonyStyle($input, $output);
+        // $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        // if ($arg1) {
+        //     $io->note(sprintf('You passed an argument: %s', $arg1));
+        // }
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
+        // if ($input->getOption('option1')) {
+        //     // ...
+        // }
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        // $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        $step = $this->cache->get('app.current_step', function ($item) {
+            $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
+            $process->mustRun();
+            $item->expiresAfter(30);
+
+            return $process->getOutput();
+        });
+        $output->writeln($step);
 
         return Command::SUCCESS;
     }
